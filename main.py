b08a28207549ec7c0c6efd89c3794546c765802b
Shio3001 <taku2000maru@icloud.com>
2021-04-06 19:05:11 +0900

    ちょっとかいた

---
+++ main.py
@@ -0,0 +1,72 @@
+from pydub import AudioSegment
+from pydub.playback import play
+import os, sys, json
+
+from linebot import LineBotApi
+from linebot.models import TextSendMessage
+
+from flask import Flask, request, abort
+ 
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import (
+    MessageEvent, TextMessage, TextSendMessage,
+)
+
+audio = os.listdir("midnightEXP")
+
+print(audio)
+
+AudioData = {}
+
+for i in audio:
+    if i[-3:] != "mp3":
+        print("ダメ")
+        continue
+
+    AudioData[i.replace(".mp3")] = AudioSegment.from_mp3(os.path.join("midnightEXP" ,i))
+
+
+
+
+app = Flask(__name__)
+ 
+#環境変数取得
+# LINE Developersで設定されているアクセストークンとChannel Secretをを取得し、設定します。
+YOUR_CHANNEL_ACCESS_TOKEN = os.environ["YOUR_CHANNEL_ACCESS_TOKEN"]
+YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
+ 
+line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
+handler = WebhookHandler(YOUR_CHANNEL_SECRET)
+
+@app.route("/callback", methods=['POST'])
+def callback():
+   signature = request.headers['X-Line-Signature']
+   body = request.get_data(as_text=True)
+   app.logger.info("Request body: " + body)
+   try:
+       handler.handle(body, signature)
+   except InvalidSignatureError:
+       abort(400)
+   return 'OK'
+   
+#メッセージに反応
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+   line_bot_api.reply_message(event.reply_token,TextSendMessage(text='再生'))
+
+   play(AudioData[event.message.text])
+
+#友達追加イベントらしい
+@handler.add(FollowEvent) #これで受信して
+def handle_follow(event): #ラインAPIの返信機能関数に
+   line_bot_api.reply_message(event.reply_token,TextSendMessage(text='特急ミッドナイトEXP松山 松山→新居浜'))
+
+
+if __name__ == "__main__":
+   port = int(os.getenv("PORT"))
+   app.run(host="0.0.0.0", port=port)
