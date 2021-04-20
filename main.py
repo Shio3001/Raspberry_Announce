0aea7c63afad4148115ecad17017b71f1ba46cfa
Shio3001 <taku2000maru@icloud.com>
2021-04-20 13:17:59 +0900

    200

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from pydub import AudioSegment
 from pydub.playback import play
-import os, sys, json
+import os
+import sys
+import json
 
 from linebot import LineBotApi
 from linebot.models import TextSendMessage
 
 from flask import Flask, request, abort
- 
+
 from linebot import (
     LineBotApi, WebhookHandler
 )
@@ -14,7 +16,7 @@ from linebot.exceptions import (
     InvalidSignatureError
 )
 from linebot.models import (
-    MessageEvent, TextMessage, TextSendMessage,FollowEvent,
+    MessageEvent, TextMessage, TextSendMessage, FollowEvent,
 )
 
 audio = os.listdir("midnightEXP")
@@ -28,52 +30,58 @@ for i in audio:
         print("ダメ")
         continue
 
-    AudioData[i.replace(".mp3","")] = AudioSegment.from_mp3(os.path.join("midnightEXP" ,i))
-
-
-
+    AudioData[i.replace(".mp3", "")] = AudioSegment.from_mp3(os.path.join("midnightEXP", i))
 
 
 app = Flask(__name__)
- 
-#環境変数取得
+
+# 環境変数取得
 # LINE Developersで設定されているアクセストークンとChannel Secretをを取得し、設定します。
 
 print(os.environ["CHANNEL_ACCESS_TOKEN"])
 print(os.environ["CHANNEL_SECRET"])
 YOUR_CHANNEL_ACCESS_TOKEN = os.environ["CHANNEL_ACCESS_TOKEN"]
 YOUR_CHANNEL_SECRET = os.environ["CHANNEL_SECRET"]
- 
+
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
+
+
 @app.route("/")
 def root():
     return "open"
 
+
 @app.route("/callback", methods=['POST'])
 def callback():
-   signature = request.headers['X-Line-Signature']
-   body = request.get_data(as_text=True)
-   app.logger.info("Request body: " + body)
-   try:
-       handler.handle(body, signature)
-   except InvalidSignatureError:
-       abort(200)
-   return 'OK'
+    signature = request.headers['X-Line-Signature']
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(200)
+    return 'OK'
 
-#メッセージに反応
+# メッセージに反応
+
+
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-   line_bot_api.reply_message(event.reply_token,TextSendMessage(text='再生'))
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text='再生'))
 
-   play(AudioData[event.message.text])
+    play(AudioData[event.message.text])
 
-#友達追加イベントらしい
-@handler.add(FollowEvent) #これで受信して
-def handle_follow(event): #ラインAPIの返信機能関数に
-   line_bot_api.reply_message(event.reply_token,TextSendMessage(text='特急ミッドナイトEXP松山 松山→新居浜'))
+    abort(200)
+
+# 友達追加イベントらしい
+
+
+@handler.add(FollowEvent)  # これで受信して
+def handle_follow(event):  # ラインAPIの返信機能関数に
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text='特急ミッドナイトEXP松山 松山→新居浜'))
 
 
 if __name__ == "__main__":
-   port = int(os.getenv("PORT","5000"))
-   app.run(host="0.0.0.0", port=port)
+    port = int(os.getenv("PORT", "5000"))
+    app.run(host="0.0.0.0", port=port)
