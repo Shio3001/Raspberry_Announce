cf6af1d0563b5e372b99e3b742f91b657c26f77b
Shio3001 <taku2000maru@icloud.com>
2021-04-22 16:54:16 +0900

    Switch

---
+++ main2.py
@@ -0,0 +1,37 @@
+from pydub import AudioSegment
+from pydub.playback import play
+
+import os
+import sys
+import json
+
+import RPi.GPIO as GPIO # RPi.GPIOモジュールを使用
+
+GPIO.setup(2,GPIO.IN)   #BCM 2番ピンを入力に設定します。                                                      
+
+audio = os.listdir("midnightEXP")
+
+print(audio)
+
+AudioData = {}
+
+# ngrokのURL末尾には/callbackをつけること
+
+for i in audio:
+    if i[-3:] != "mp3":
+        print("ダメ")
+        continue
+
+    AudioData[i.replace(".mp3", "")] = AudioSegment.from_mp3(os.path.join("midnightEXP", i))
+
+counter = 0
+
+try:
+        while True:
+                if GPIO.input(2) == GPIO.HIGH:
+                    play(counter)
+
+                    counter += 1
+                    
+except KeyboardInterrupt:
+        GPIO.cleanup()
