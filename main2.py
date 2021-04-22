362f153e6aca9f6f5f54c632f2cd9ecae11b1a70
Shio3001 <taku2000maru@icloud.com>
2021-04-22 17:20:45 +0900

    Switch9

--- main2.py
+++ main2.py
@@ -7,6 +7,8 @@ import json
 
 import RPi.GPIO as GPIO # RPi.GPIOモジュールを使用
 
+import time
+
 GPIO.setmode(GPIO.BCM)  #GPIOへアクセスする番号をBCMの番号で指定することを宣言します。                        
 GPIO.setup(2,GPIO.IN)   #BCM 2番ピンを入力に設定します。                                                      
 
